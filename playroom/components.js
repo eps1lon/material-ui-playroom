f2da47fb10b7035da4606e150fe339a52bd2a53e
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-05-10 16:53:42 +0200

    feat: Include demos as components (#2)
    * Include app-bar demos
    
    * Include demos statically with a update script
    
    * Fix cannot read property propTypes of undefined

--- playroom/components.js
+++ playroom/components.js
@@ -1,7 +1,15 @@
+const babel = require("@babel/core");
 const components = require("@material-ui/core");
 const MenuIcon = require("@material-ui/icons/Menu").default;
 
+require("@babel/register")({
+  presets: ["@babel/preset-env", "@babel/preset-react"],
+  plugins: ["@babel/plugin-proposal-class-properties"]
+});
+const demos = require("./demos");
+
 module.exports = {
   ...components,
+  ...demos,
   MenuIcon
 };
