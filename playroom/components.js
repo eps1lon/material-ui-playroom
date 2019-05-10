933e72a4818d21711e0352d09a1548b6fd6dfcca
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-05-10 15:30:05 +0200

    Include app-bar demos

--- playroom/components.js
+++ playroom/components.js
@@ -1,7 +1,26 @@
+const babel = require("@babel/core");
+const glob = require("glob");
+const path = require("path");
 const components = require("@material-ui/core");
 const MenuIcon = require("@material-ui/icons/Menu").default;
 
+require("@babel/register")({
+  presets: ["@babel/preset-env", "@babel/preset-react"],
+  plugins: ["@babel/plugin-proposal-class-properties"]
+});
+// poor mans require.context
+const demoDirectory = path.join(
+  __dirname,
+  "../material-ui/docs/src/pages/components/app-bar"
+);
+const demos = Object.fromEntries(
+  glob.sync(path.join(demoDirectory, "**/*.js")).map(demoFilename => {
+    return [path.basename(demoFilename, ".js"), require(demoFilename).default];
+  })
+);
+
 module.exports = {
   ...components,
+  ...demos,
   MenuIcon
 };
